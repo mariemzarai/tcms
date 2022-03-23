7f792d1b897e7089ae0e900c8a46f686a9b5bb0f
Yassine Mejri <yassinemejri5@gmail.com>
2022-03-23 21:20:29 +0100

    initial commit

---
+++ src/main/java/com/restapi/tcms/TcmsApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.tcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcmsApplication.class, args);
+	}
+
+}
